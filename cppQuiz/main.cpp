604294e35fcd2d29af47a62c5a749c36159cc250
Finlay Fehlauer <f.fehlauer@schueler.lg-ks.de>
2021-05-27 15:26:59 +0200

    Input sanitization

--- cppQuiz/main.cpp
+++ cppQuiz/main.cpp
@@ -11,14 +11,34 @@ int main() {
 		for (int i = 0; i < quiz->getQuestionCount(); i++)
 		{
 			const cpp_quiz::Question question = quiz->getQuestionAtIndex(i);
-			int answer;
+			std::string input;
 			std::cout << question.getTitle() << "\n";
 			for (int j = 0; j < question.getOptionCount(); j++)
 			{
 				std::cout << j << ") " << question.getItemAtIndex(j) << "\n";
 			}
-			std::cout << "Your answer: ";
-			std::cin >> answer;
+			int answer = -1;
+
+			// Check whether answer is integer
+			while (answer == -1)
+			{
+				std::cout << "Your answer (number between 0 and " << question.getOptionCount()-1 << "): ";
+				std::cin >> input;
+				try {
+					answer = std::stoi(input);
+
+					if (answer < 0 || answer >= question.getOptionCount())
+					{
+						std::cout << "Your answer need to be between 0 and " << question.getOptionCount() - 1 << ". Please try again.\n";
+						answer = -1;
+					}
+				}
+				catch (std::exception& e) { // Not a number
+					std::cout << "Your answer need to be a number between 0 and " << question.getOptionCount() - 1 << ". Please try again.\n";
+					answer = -1;
+				}
+			}
+
 			if (question.isCorrect(answer)) {
 				score += 1;
 				std::cout << "CORRECT\n";
